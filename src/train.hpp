73c924777af461276bda290d21adc07c41a1ae20
pigstd <zhuzemu@sjtu.edu.cn>
2025-05-29 12:28:15 +0800

    完成 query_ticket，未测试

--- src/train.hpp
+++ src/train.hpp
@@ -4,12 +4,18 @@
 #include "MR_with_cache.hpp"
 #include "database.hpp"
 #include "operator.hpp"
+#include "priority_queue.hpp"
 #include "utility.hpp"
 #include "time.hpp"
+#include "vector.hpp"
+#include <cassert>
+#include <functional>
 #include <string>
 
 using std::string;
 using sjtu::pair;
+using sjtu::vector;
+using sjtu::priority_queue;
 
 typedef mystr<20> trainstr;
 typedef mystr<30> stationstr;
@@ -97,6 +103,68 @@ public:
             if (stations[i] == station) return i;
         return -1;
     }
+    /*
+    找到在 nowDate 当天，在 nowtime 之后，这辆车从 FROMID 到 TOID 站的信息，按照 query_ticket 的格式
+
+    若 can_wait，那么可以等到下一天
+
+    其中，FROMID 和 TOID 都是这个站在这辆车的 id（可以用 findstation 找到）
+
+    1 <= FROMID < TOID <= stationNum
+    
+    返回的 vector : <trainID> <FROM> <LEAVING_TIME> -> <TO> <ARRIVING_TIME> <PRICE> <SEAT>
+    
+    如果这辆车时间不满足，返回空的 vector，否则 vector 返回的分别是上述的 9 个参数（LEAVING_TIME 分为日期和时间）以及最后的总的字符串
+    */
+    vector<string> query_ticket(int FROMID, int TOID, date nowDate, Time starttime = Time(), bool can_wait = false) {
+        assert(1 <= FROMID && FROMID < TOID && TOID <= stationNum);
+        vector<string> res;
+        // 先算出从出发到 FROMID 站需要的时间
+        Time nowtime = startTime;
+        for (int i = 1; i < FROMID; i++) {
+            nowtime += travelTimes[i];
+            nowtime += stopoverTimes[i + 1];
+        }
+        // 需要提前 nowtime.day 天出发
+        for (int i = 1; i <= nowtime.day; i++) nowDate--;
+        // 这辆车到站的时间是 nowtime.hour, nowtime.minute
+        // 如果比 starttime 小，那么一定是可以等的情况，并且这个时候要再迟一天
+        if (nowtime.hour < starttime.hour ||
+            (nowtime.hour == starttime.hour && nowtime.minute < starttime.minute)) {
+            assert(can_wait);
+            nowDate++;
+        }
+        // 这个时候的 nowDate 是这辆车应该出发的日期（如果可以等，就是至少出发的日期）
+        if (nowDate > saleend) return res;
+        if (nowDate < salebegin) {
+            if (can_wait) nowDate = salebegin;
+            else return res;
+        }
+        int dayid = int(nowDate) - int(salebegin);
+        res.push_back(trainID);
+        res.push_back(stations[FROMID]);
+        // 再改回在 FROMID 的时候的日期和时间
+        while(nowtime.day) nowtime.day--, nowDate++;
+        res.push_back(string(nowDate));
+        res.push_back(string(nowtime));
+        int sumprice = 0, maxseat = seatNum;
+        for (int i = FROMID; i < TOID; i++) {
+            nowtime += travelTimes[i];
+            if (i + 1 != TOID) nowtime += stopoverTimes[i + 1];
+            sumprice += prices[i];
+            int seat = remain_seat[dayid][i];
+            if (seat < maxseat) maxseat = seat;
+        }
+        while(nowtime.day) nowtime.day--, nowDate++;
+        res.push_back(stations[TOID]);
+        res.push_back(string(nowDate));
+        res.push_back(string(nowtime));
+        res.push_back(std::to_string(sumprice));
+        res.push_back(std::to_string(maxseat));
+        string S = res[0] + " " + res[1] + " " + res[2] + " " + res[3] + " -> "
+        + res[4] + " " + res[5] + " " + res[6] + " " + res[7] + res[8] + '\n';
+        return res;
+    }
     // TO DO
 };
 
@@ -164,11 +232,45 @@ public:
         if (!(train.salebegin <= Date && Date <= train.saleend)) return "-1";
         return train.query_train(Date);
     }
-    string query_ticket(const OP &cmd) {
-        // TO DO!!
+    string query_ticket(OP &cmd) {
+        cmd.setdefault('p', "time");
+        date Date       = cmd.key('d');
+        stationstr FROM = cmd.key('s');
+        stationstr TO   = cmd.key('t');
+        string method   = cmd.key('p');
+        auto trainindex = station_to_stationdat->find_with_vector({FROM, TO});
+        // 用于排序的优先队列
+        priority_queue<pair<pair<int, trainstr>, string>,
+            std::greater<pair<pair<int, trainstr>, string>>> Q;
+        for (auto index : trainindex) {
+            Train train; traindat->read(train, index);
+            int FROMID = train.findstation(FROM);
+            int TOID   = train.findstation(TO);
+            auto info = train.query_ticket(FROMID, TOID, Date);
+            if (info.empty()) continue;
+            if (method == "time") {
+                date FROMdate = info[2];
+                Time FROMtime = info[3];
+                date TOdate   = info[5];
+                Time TOtime   = info[6];
+                int tottime = calc_time_to_begin(TOdate, TOtime)
+                            - calc_time_to_begin(FROMdate, FROMtime);
+                Q.push({{tottime, train.trainID}, info[9]});
+            }
+            else {
+                int totprice = std::stoi(info[7]);
+                Q.push({{totprice, train.trainID}, info[9]});
+            }
+        }
+        string res = std::to_string(Q.size()) + "\n";
+        while(!Q.empty()) {
+            auto u = Q.top(); Q.pop();
+            res += u.second;
+        }
+        return res;
     }
-    string query_transfer(const OP &cmd) {
-        // TO DO!!
+    string query_transfer(OP &cmd) {
+        
     }
     // TO DO
 };
