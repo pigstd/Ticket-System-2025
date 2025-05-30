13e0e8cdf73ec8c5fb774bbc75451c126819ac32
pigstd <zhuzemu@sjtu.edu.cn>
2025-05-30 21:53:18 +0800

    MemoryRiver 修改：只在 init, clear 和 析构的时候对文件进行打开和关闭，其他每次操作不重新打开关闭

--- src/MemoryRiver.hpp
+++ src/MemoryRiver.hpp
@@ -40,6 +40,7 @@ public:
                 file.seekg(i * sizeof(int), std::ios::beg),
                 file.read(reinterpret_cast<char *>(&a[i]), sizeof(int));
             file.close();
+            file.open(file_name, std::ios::out | std::ios::in);
             return;
         }
         // std::cerr << "cover: " << file_name << '\n';
@@ -50,6 +51,7 @@ public:
         for (int i = 0; i < info_len; ++i)
             file.write(reinterpret_cast<char *>(&num), sizeof(int)), a[i + 1] = num;
         file.close();
+        file.open(file_name, std::ios::out | std::ios::in);
     }
 
     //读出第n个int的值赋给tmp，1_base
@@ -76,7 +78,7 @@ public:
     //位置索引意味着当输入正确的位置索引index，在以下三个函数中都能顺利的找到目标对象进行操作
     //位置索引index可以取为对象写入的起始位置
     int write(T &t) {
-        file.open(file_name, std::ios::out | std::ios::in);
+        // file.open(file_name, std::ios::out | std::ios::in);
         int head; get_info(head, 0);
         if (head == 0) file.seekp(0, std::ios::end);
         else {
@@ -87,16 +89,16 @@ public:
         }
         int index = file.tellp();
         file.write(reinterpret_cast<char *>(&t), sizeof(T));
-        file.close();
+        // file.close();
         // mp[index] = 1;
         return index;
     }
     //用t的值更新位置索引index对应的对象，保证调用的index都是由write函数产生
     void update(T &t, const int index) {
-        file.open(file_name, std::ios::out | std::ios::in);
+        // file.open(file_name, std::ios::out | std::ios::in);
         file.seekp(index, std::ios::beg);
         file.write(reinterpret_cast<char *>(&t), sizeof(T));
-        file.close();
+        // file.close();
     }
 
     //读出位置索引index对应的T对象的值并赋值给t，保证调用的index都是由write函数产生
@@ -105,28 +107,28 @@ public:
         //     std::cerr << "error index: " << index << '\n';
         //     assert(0);
         // }
-        file.open(file_name, std::ios::in);
+        // file.open(file_name, std::ios::in);
         file.seekg(index,std::ios::beg);
         file.read(reinterpret_cast<char *>(&t), sizeof(T));
-        file.close();
+        // file.close();
     }
 
     //删除位置索引index对应的对象(不涉及空间回收时，可忽略此函数)，保证调用的index都是由write函数产生
     void Delete(int index) {
         // if (mp.find(index) == mp.end()) assert(0);
         // mp.erase(mp.find(index));
-        file.open(file_name, std::ios::out | std::ios::in);
+        // file.open(file_name, std::ios::out | std::ios::in);
         int head; get_info(head, 0);
         write_info(index, 0);
         file.seekp(index, std::ios::beg);
         file.write(reinterpret_cast<char *>(&head), sizeof(int));
-        file.close();
+        // file.close();
     }
     void clear(string FN, int num = 0) {
+        file.close();
         initialise(FN, num, 1);
     }
     ~MemoryRiver() {
-        file.open(file_name, std::ios::out | std::ios::in);
         for (int i = 0; i <= info_len; i++)
             file.seekp(i * sizeof(int), std::ios::beg),
             file.write(reinterpret_cast<char *>(&a[i]), sizeof(int));
